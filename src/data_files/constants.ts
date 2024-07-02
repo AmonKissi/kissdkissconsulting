9f452a8135f20dc4fa4617717e42896b4c970fe5
AmonKissi <amondkissi@gmail.com>
2024-07-01 23:41:55 -0500

    fixing metadata

--- src/data_files/constants.ts
+++ src/data_files/constants.ts
@@ -1,27 +1,29 @@
-import ogImageSrc from "@images/social.png";
+import ogImageSrc from '@images/social.png';
 
 export const SITE = {
   title: "Kiss D'Kiss",
-  tagline: "Study Abroad",
-  description: "Empowering students from around the world to pursue their dreams of studying in the U.S.",
-  description_short: "Empowering students from around the world to pursue their dreams of studying in the U.S.",
-  url: "https://kissdkiss.com",
-  author: "Amon Kissi",
+  tagline: 'Study Abroad',
+  description:
+    'Empowering students from around the world to pursue their dreams of studying in the U.S.',
+  description_short:
+    'Empowering students from around the world to pursue their dreams of studying in the U.S.',
+  url: 'https://kissdkiss.com',
+  author: 'Amon Kissi',
 };
 
 export const SEO = {
   title: SITE.title,
   description: SITE.description,
   structuredData: {
-    "@context": "https://schema.org",
-    "@type": "WebPage",
-    inLanguage: "en-US",
-    "@id": SITE.url,
+    '@context': 'https://schema.org',
+    '@type': 'WebPage',
+    inLanguage: 'en-US',
+    '@id': SITE.url,
     url: SITE.url,
     name: SITE.title,
     description: SITE.description,
     isPartOf: {
-      "@type": "WebSite",
+      '@type': 'WebSite',
       url: SITE.url,
       name: SITE.title,
       description: SITE.description,
@@ -30,10 +32,11 @@ export const SEO = {
 };
 
 export const OG = {
-  locale: "en_US",
-  type: "website",
+  locale: 'en_US',
+  type: 'website',
   url: SITE.url,
-  title: `${SITE.title}: : Empowering students from around the world to pursue their dreams of studying in the U.S.`,
-  description: "Permettre aux étudiants du monde entier de poursuivre leurs rêves d'étudier aux États-Unis.",
+  title: `${SITE.title}: Empowering students from around the world to pursue their dreams of studying in the U.S.`,
+  description:
+    "Permettre aux étudiants du monde entier de poursuivre leurs rêves d'étudier aux États-Unis.",
   image: ogImageSrc,
 };
